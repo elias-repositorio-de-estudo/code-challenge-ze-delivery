9231a79ced17cb9cf376ba39ed48fd3fbec6b7c3
Elias Ribeiro <eliasctrl@live.com>
2020-10-20 23:31:10 -0300

    create model

---
+++ src/main/java/br/com/xyz/zedelivery/model/Coordinates.java
@@ -0,0 +1,11 @@
+package br.com.xyz.zedelivery.model;
+
+import lombok.*;
+
+@AllArgsConstructor
+@Getter
+public class Coordinates {
+
+    private final Long longitude;
+    private final Long latitude;
+}
---
+++ src/main/java/br/com/xyz/zedelivery/model/PDV.java
@@ -0,0 +1,24 @@
+package br.com.xyz.zedelivery.model;
+
+
+import lombok.Getter;
+import org.hibernate.validator.constraints.br.CNPJ;
+import org.locationtech.jts.geom.*;
+
+import javax.persistence.*;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Getter
+public class PDV {
+    @Id @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+    private String tradingName;
+    private String ownerName;
+    @CNPJ
+    @Column(unique = true)
+    private String document;
+    private MultiPolygon coverageArea;
+    private Point address;
+}
