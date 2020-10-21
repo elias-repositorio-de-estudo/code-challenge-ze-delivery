6937523ea4b7af721aa72a167a57f0241f4117a9
Elias Ribeiro <eliasctrl@live.com>
2020-10-20 23:11:58 -0300

    application setup

---
+++ src/main/java/br/com/xyz/zedelivery/ZeDeliveryApplication.java
@@ -0,0 +1,13 @@
+package br.com.xyz.zedelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZeDeliveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/xyz/zedelivery/ZeDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.xyz.zedelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
